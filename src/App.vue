360cc1771df7a50e71d6a89b05bbea1dd74de3a8
Tima Baisalov <baisalov.t98@gmail.com>
2025-03-09 11:57:12 -0500

    black square added

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="black-square"></div>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: white;
+}
+
+.black-square {
+  width: 200px;
+  height: 200px;
+  background-color: black;
+}
+</style>
